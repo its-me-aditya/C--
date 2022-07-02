56732c35fe393b74d094bf7516bdbb4e13de1d8b
Aditya Singh <adityas321btechcse2025@kccitm.edu.in>
2022-07-02 16:36:19 +0530

    additon.cpp

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a;
+    int b;
+    cout << "enter the first number:";
+    
+    
+    cin >> a;
+    cout << " enter the second number:";
+    cin >> b;
+    int c = a+b ;
+    cout << "total of number:";
+    cout << c << endl;
+    
+}
