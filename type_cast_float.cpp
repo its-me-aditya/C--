3a376619df61afaf77a56ad742f66ea006d7e9f8
Aditya Singh <adityas321btechcse2025@kccitm.edu.in>
2022-07-03 15:35:05 +0530

    type_cast_float.cpp

---
+++ type_cast_float.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    a=1990;
+    c=a;
+    cout <<c<<endl;
+    c= 'y';
+    a=c;
+    cout<<a<< endl;
+    float f =1.2;
+    a=f;
+    cout<<a >> endl;
+}
