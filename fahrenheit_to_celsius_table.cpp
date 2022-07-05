60258ff8069bdf79c364287e58c64170f31a4b48
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2022-07-05 22:46:36 +0530

    Create fahrenheit_to_celsius_table.cpp

---
+++ fahrenheit_to_celsius_table.cpp
@@ -0,0 +1,16 @@
+  #include <iostream>
+using namespace std;
+
+int main(){
+    int S,E,W;
+    cin >> S >> E>> W;
+    int C;
+    
+    while (S<=E){
+        C=5.0/9*(S-32);
+        cout << S << "\t" << C<<endl;
+        S=S+W;
+    }
+}
+
+  
