81c44ce75b7de14ab0d981f457c8bb67066a62e4
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2022-07-12 22:11:37 +0530

    Create triangle_of_num.cpp

---
+++ triangle_of_num.cpp
@@ -0,0 +1,32 @@
+
+using namespace std;
+
+int main() {
+   
+      int n;
+    cin >>n;
+    int i = 1;
+    while (i <= n) {
+        int space = 1;
+        while( space <= n - i) {
+            cout << ' ';
+            space = space + 1;
+         } 
+         int j = 1;
+         	int m = i;
+            while ( j <= i ) {
+            
+            cout << m;
+            m = m + 1;
+            j = j ;
+        }
+        int k = i;
+        while(k > 1) {
+        	cout <<k;
+        	k = k - 1;
+		}
+              cout << endl;
+        i = i + 1;
+    }
+    
+}
